9fdb8f2d159ff1ba1e0ca27d745f75a7bd9c5a87
deehood <dee.hood@gmail.com>
2022-07-16 20:08:14 +0100

    .

--- library/library-backend.js
+++ library/library-backend.js
@@ -101,7 +101,7 @@ const typeDefs = gql`
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -110,7 +110,7 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.find((book) => book.author === args.author),
     allAuthors: () => authors,
   },
 
