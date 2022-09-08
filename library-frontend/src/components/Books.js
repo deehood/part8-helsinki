bb418252658c306c563f7778e70ebda36c504e90
deehood <dee.hood@gmail.com>
2022-09-08 14:01:35 +0100

    choosing genre stops rerendering genes tab

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -9,16 +9,18 @@ const Books = (props) => {
     if (!props.show) {
         return null;
     }
-    if (result.loading) {
-        return <div>loading...</div>;
-    }
+    // if (result.loading) {
+    //     return <div>loading...</div>;
+    // }
 
     if (result.error) return <>{result.error.message}</>;
-    if (uniqueGenres.current === null) {
+
+    if (uniqueGenres.current === null && result.data) {
         uniqueGenres.current = Array.from(
             new Set(result.data.allBooks.map((obj) => obj.genres).flat())
         );
     }
+    // }
     // solution for adding book
     result.refetch();
     return (
@@ -26,7 +28,6 @@ const Books = (props) => {
             <h2>books</h2>
             <b>Genres</b>
             <br />
-
             {["All", ...uniqueGenres.current].map((mappedGenre, index) => (
                 <button
                     className={mappedGenre === genre ? "selected-genre" : null}
@@ -36,34 +37,34 @@ const Books = (props) => {
                     {mappedGenre}
                 </button>
             ))}
-
-            <table>
-                <tbody>
-                    <tr>
-                        <th>books</th>
-                        <th>author</th>
-                        <th>published</th>
-                        <th>genres</th>
-                    </tr>
-                    {result.loading === true} ? null :
-                    {result.data.allBooks.map((x) => (
-                        <tr key={x.title}>
-                            <td>{x.title}</td>
-                            <td>{x.author.name}</td>
-                            <td>{x.published}</td>
-                            <td>
-                                {x.genres.map(
-                                    (a, index) =>
-                                        a +
-                                        (index + 1 === x.genres.length
-                                            ? ""
-                                            : ", ")
-                                )}
-                            </td>
+            {result.loading === true ? null : (
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>books</th>
+                            <th>author</th>
+                            <th>published</th>
+                            <th>genres</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                        {result.data.allBooks.map((x) => (
+                            <tr key={x.title}>
+                                <td>{x.title}</td>
+                                <td>{x.author.name}</td>
+                                <td>{x.published}</td>
+                                <td>
+                                    {x.genres.map(
+                                        (a, index) =>
+                                            a +
+                                            (index + 1 === x.genres.length
+                                                ? ""
+                                                : ", ")
+                                    )}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
     );
 };
